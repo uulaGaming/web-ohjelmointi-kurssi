3b6c686c827fe7c9a7ce14c973606106557bef36
Uula <us3411@edu.turku.fi>
2026-04-13 12:03:06 +0300

    bad javascript page added

---
+++ portfolio/muuttujat.js
@@ -0,0 +1,17 @@
+let a = 3;
+let b = 5;
+let c = 7;
+let d = 15;
+
+function summa() {
+  alert( a + b );
+}
+function vahennys() {
+  alert( b - c );
+}
+function kerto() {
+  alert( c * d );
+}
+function jako() {
+  alert( d * a );
+}
