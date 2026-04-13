6c184c68b91024f6989e49bb6f02d59c315e7fd1
Uula <us3411@edu.turku.fi>
2026-04-13 14:10:53 +0300

    laskin

--- portfolio/muuttujat.js
+++ portfolio/muuttujat.js
@@ -3,15 +3,41 @@ let b = 5;
 let c = 7;
 let d = 15;
 
-function summa() {
-  alert( a + b );
+function summa(x,y) {
+  alert( x + y );
 }
-function vahennys() {
-  alert( b - c );
+function vahennys(x,y) {
+  alert( x - y );
 }
-function kerto() {
-  alert( c * d );
+function kerto(x,y) {
+  alert( x * y );
 }
-function jako() {
-  alert( d * a );
+function jako(x,y) {
+  alert( x * y );
+}
+
+/* kuka ylhäällä javaamassa niien skriptiä rn */
+
+function laskin_summa() {
+  let eka = Number(document.getElementById('input1').value);
+  let toka = Number(document.getElementById('input2').value);
+  summa(eka,toka);
+}
+
+function laskin_vahennys() {
+  let eka = document.getElementById('input1').value;
+  let toka = document.getElementById('input2').value;
+  vahennys(eka,toka);
+}
+
+function laskin_kerto() {
+  let eka = document.getElementById('input1').value;
+  let toka = document.getElementById('input2').value;
+  kerto(eka,toka);
+}
+
+function laskin_jako() {
+  let eka = document.getElementById('input1').value;
+  let toka = document.getElementById('input2').value;
+  jako(eka,toka);
 }
